f10fab1a13de3788f7a7d08d7b3fe1e34c728ce7
Sowmya <sowmyam0291@gmail.com>
2024-08-26 12:29:50 +0530

    git commit -m "Test commit" git branch git push origin main git --version

--- local_library/catalog/admin.py
+++ local_library/catalog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Language)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+# Register the Admin class with the associated model
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# Method to display genres
+def display_genre(self):
+    """Create a string for the Genre. This is required to display genre in Admin."""
+    return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+display_genre.short_description = 'Genre'
