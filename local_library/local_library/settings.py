0bf3b6ac990fee4893a49689c9da07057f0ea8d2
Sowmya <sowmyam0291@gmail.com>
2024-08-27 14:35:26 +0530

    Test Commit

--- local_library/local_library/settings.py
+++ local_library/local_library/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'static/'
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
