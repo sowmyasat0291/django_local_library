f95d35bb9d784f97da99d8c22964616c58d4758f
Sowmya <sowmyam0291@gmail.com>
2024-08-27 11:08:47 +0530

    Test commit

--- local_library/catalog/urls.py
+++ local_library/catalog/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.home_view, name='home'),  # Maps the root URL to home_view
-    path('about/', views.about_view, name='about'),  # Maps /about/ URL to about_view
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/<int:pk>/', views.author_detail, name='author-detail'),
+
+
+
+    #path('', views.home_view, name='home'),  # Maps the root URL to home_view
+    #path('about/', views.about_view, name='about'),  # Maps /about/ URL to about_view
 ]
--- local_library/catalog/views.py
+++ local_library/catalog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from django.views import generic
+from django.shortcuts import get_object_or_404
+
 from .models import Book, Author, BookInstance, Genre
+
+def author_detail(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, 'catalog/author_detail.html', {'author': author})
 
 def index(request):
     """View function for home page of site."""
@@ -31,6 +38,19 @@ def home_view(request):
 
 def about_view(request):
     return HttpResponse("This is the about page.")
-
-# Create your views here.
 """
+# Create your views here.
+class BookListView(generic.ListView):
+    model = Book
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+     book = get_object_or_404(Book, pk=primary_key)
+     return render(request, 'catalog/book_detail.html', context={'book': book})
+
