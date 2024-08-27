345bad78efbbbb3b4ea458b4a11f0bdca4518706
Sowmya <sowmyam0291@gmail.com>
2024-08-27 14:24:33 +0530

    Test Commit

--- local_library/local_library/settings.py
+++ local_library/local_library/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
