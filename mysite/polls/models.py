a6be1d45e390d4b25cbb9525a453c91da45c3434
Sowmya <sowmyam0291@gmail.com>
2024-08-28 13:55:39 +0530

    Test Commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,15 +4,18 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pud_date = models.DateField('date published')
+    pub_date = models.DateTimeField('date published')  # Change to DateTimeField if needed
+
     def __str__(self):
         return self.question_text
+
     def was_published_recently(self):
-        return self.pud_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Question
 def index(request):
-    return HttpResponse ("Hello, world! you are at polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ", ".join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {"question": question})
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
