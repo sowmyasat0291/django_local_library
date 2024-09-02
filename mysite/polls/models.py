d70f717304cf3e00b367ac9365bbcd9171773b7d
Sowmya <sowmyam0291@gmail.com>
2024-09-02 14:22:16 +0530

    Test Commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,10 +1,16 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')  # Change to DateTimeField if needed
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
 
     def __str__(self):
         return self.question_text
